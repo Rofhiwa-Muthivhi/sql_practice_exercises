19d3d8da532ca42098031d751338f68454559096
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-04-24 10:29:08 +0200

    Created folder structure for exercise 5 to store create table script and solutions seperately

---
+++ 05_date_functions/solutions/create_database/create_db.sql
@@ -0,0 +1,8 @@
+-- Exercise 5
+-- DATE Funtions
+
+-- Creating database for for date functions
+CREATE DATABASE Bright_Date_Functions;
+
+-- How to ensure the database is default for this exercise
+USE Bright_Date_Functions;
